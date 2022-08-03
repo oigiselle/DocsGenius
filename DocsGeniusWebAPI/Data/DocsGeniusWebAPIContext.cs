86e6d2787b106da530a2e81182d770f99f3deb9d
oigiselle <gisellesalvadorx@gmail.com>
2022-08-02 23:58:48 -0300

    API controller was implemented, however it is not complete - is presenting errors. API will be developed after all the models were created and tested.

---
+++ DocsGeniusWebAPI/Data/DocsGeniusWebAPIContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DocsGenius.Models;
+
+namespace DocsGeniusWebAPI.Data
+{
+    public class DocsGeniusWebAPIContext : DbContext
+    {
+        public DocsGeniusWebAPIContext (DbContextOptions<DocsGeniusWebAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<DocsGenius.Models.DocsTemplate> DocsTemplate { get; set; }
+    }
+}
